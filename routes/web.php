0593819a55e6a5fbceb183cd54a4c48451170185
Shobhnath-I2A <shobhnath.s@i2a.co>
2026-04-24 20:07:46 +0530

    add login system

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ActivityRateController;
 use App\Http\Controllers\LeadController;
@@ -24,88 +26,90 @@ use App\Http\Controllers\PackageThemeController;
 use App\Http\Controllers\WeatherSettingController;
 use App\Http\Controllers\CurrencyExchangeMasterController;
 use App\Http\Controllers\NotificationController;
+use App\Events\LeadNotificationCreated;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('login');
 });
 
-Route::get('/queries',[QueryController::class,'index'])->name('queries.index');
-Route::get('/queries/create',[QueryController::class,'create'])->name('queries.create');
-Route::post('/queries',[QueryController::class,'store'])->name('queries.store');
-Route::get('/queries/{id}/edit',[QueryController::class,'edit'])->name('queries.edit');
-Route::put('/queries/{id}',[QueryController::class,'update'])->name('queries.update');
-Route::get('/queries/{id}', [QueryController::class, 'show'])->name('queries.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::resource('query-tasks', QueryTaskController::class);
-Route::get('/check-reminders', [QueryTaskController::class, 'checkReminders']);
-Route::post('/task-done/{id}', [QueryTaskController::class, 'markDone']);
+    Route::get('/queries', [QueryController::class, 'index'])->name('queries.index');
+    Route::get('/queries/create', [QueryController::class, 'create'])->name('queries.create');
+    Route::post('/queries', [QueryController::class, 'store'])->name('queries.store');
+    Route::get('/queries/{id}/edit', [QueryController::class, 'edit'])->name('queries.edit');
+    Route::put('/queries/{id}', [QueryController::class, 'update'])->name('queries.update');
+    Route::get('/queries/{id}', [QueryController::class, 'show'])->name('queries.show');
 
-Route::resource('clients', ClientController::class);
-// Route::resource('package-query', PackageQueryController::class);
-Route::resource('package-query', LeadController::class);
+    Route::resource('query-tasks', QueryTaskController::class);
+    Route::get('/check-reminders', [QueryTaskController::class, 'checkReminders']);
+    Route::post('/task-done/{id}', [QueryTaskController::class, 'markDone']);
 
-Route::resource('/itineraries', ItineraryController::class);
-Route::get('/itinerary/day-details', [ItineraryController::class, 'getDayDetails'])
-    ->name('itinerary.day.details');
-Route::get('/itinerary/acccomodation', [ItineraryController::class, 'createAccomodation'])
-    ->name('itinerary.day.accomodation');
-Route::post('/itinerary/store-acccomodation', [ItineraryController::class, 'storeAccomodation'])
-    ->name('itinerary.storeaccomodation');
-Route::get('/load-hotels', [ItineraryController::class, 'loadHotels']);
-Route::get('/load-hotel-data', [ItineraryController::class, 'loadHotelData']);
+    Route::resource('clients', ClientController::class);
+    Route::resource('package-query', LeadController::class);
 
-Route::resource('hotels', HotelController::class);
-Route::resource('hotels-rates', HotelRateController::class);
-Route::get('/get-hotels/{destination}', [HotelController::class, 'getHotels']);
+    Route::resource('itineraries', ItineraryController::class);
+    Route::get('/itinerary/day-details', [ItineraryController::class, 'getDayDetails'])->name('itinerary.day.details');
+    Route::get('/itinerary/acccomodation', [ItineraryController::class, 'createAccomodation'])->name('itinerary.day.accomodation');
+    Route::post('/itinerary/store-acccomodation', [ItineraryController::class, 'storeAccomodation'])->name('itinerary.storeaccomodation');
 
-Route::resource('room-type', HotelRoomTypeController::class);
+    Route::get('/load-hotels', [ItineraryController::class, 'loadHotels']);
+    Route::get('/load-hotel-data', [ItineraryController::class, 'loadHotelData']);
 
-Route::resource('activities', ActivityController::class);
-Route::resource('activities-rates', ActivityRateController::class);
+    Route::resource('hotels', HotelController::class);
+    Route::resource('hotels-rates', HotelRateController::class);
+    Route::get('/get-hotels/{destination}', [HotelController::class, 'getHotels']);
 
-Route::resource('destinations', DestinationController::class);
+    Route::resource('room-type', HotelRoomTypeController::class);
 
-Route::resource('package-days-items', PackageDaysItemController::class);
+    Route::resource('activities', ActivityController::class);
+    Route::resource('activities-rates', ActivityRateController::class);
 
-Route::get('/get-master-data', [PackageDaysItemController::class, 'getMasterData']);
+    Route::resource('destinations', DestinationController::class);
+    Route::resource('package-days-items', PackageDaysItemController::class);
+    Route::get('/get-master-data', [PackageDaysItemController::class, 'getMasterData']);
 
-Route::resource('transfers', TransferMasterController:: class);
+    Route::resource('transfers', TransferMasterController::class);
+    Route::resource('transfer-rate-list', TransferMasterRateListController::class);
 
-Route::resource('transfer-rate-list', TransferMasterRateListController::class);
+    Route::resource('settings', SettingController::class);
+    Route::resource('meal-plan-master', MealPlanMasterController::class);
+    Route::resource('day-itinerary-master', DayItineraryMasterController::class);
+    Route::resource('lead-source', LeadSourceController::class);
+    Route::resource('package-theme', PackageThemeController::class);
+    Route::resource('weather-setting', WeatherSettingController::class);
+    Route::resource('currency-exchange', CurrencyExchangeMasterController::class);
 
-Route::resource('settings', SettingController::class);
-// Route::get('settings', [SettingController::class, 'show'])->name('settings.show');
-Route::resource('meal-plan-master', MealPlanMasterController::class);
-Route::resource('day-itinerary-master', DayItineraryMasterController::class);
-Route::resource('lead-source', LeadSourceController::class);
-Route::resource('package-theme', PackageThemeController::class);
-Route::resource('weather-setting', WeatherSettingController::class);
-Route::resource('currency-exchange', CurrencyExchangeMasterController::class);
-
-
-
-// Route::middleware(['auth'])->group(function () {
     Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
     Route::get('/notifications/latest', [NotificationController::class, 'latest']);
     Route::post('/notifications/{id}/mark-read', [NotificationController::class, 'markRead']);
-// });
-use App\Events\LeadNotificationCreated;
 
+    Route::get('/trigger-test-notification', function () {
+        $notification = (object) [
+            'id' => 1,
+            'lead_id' => 123,
+            'type' => 'new_lead',
+            'title' => 'New Lead',
+            'message' => 'Test notification from route',
+            'data' => [
+                'lead_name' => 'Test User',
+                'phone' => '9999999999',
+            ],
+        ];
 
-Route::get('/trigger-test-notification', function () {
-    $notification = (object) [
-        'id' => 1,
-        'lead_id' => 123,
-        'type' => 'new_lead',
-        'title' => 'New Lead',
-        'message' => 'Test notification from route',
-        'data' => [
-            'lead_name' => 'Test User',
-            'phone' => '9999999999',
-        ],
-    ];
+        broadcast(new LeadNotificationCreated($notification, 1));
 
-    broadcast(new LeadNotificationCreated($notification, 1));
+        return 'sent';
+    });
 
-    return 'sent';
 });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
